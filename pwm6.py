8dc871a1b7dcff82d1f7e13380e77ce2e7bb1b5d
wuyopmail <wuyopmail@yopmail.com>
2017-10-27 19:50:10 +0800

    First Upload.

---
+++ pwm6.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12,GPIO.OUT)
+
+p = GPIO.PWM(12,50)
+p.start(6)
+
+#p.ChangeDutyCycle(-10)
+#time.sleep(0.1)
+i = 45
+p.ChangeDutyCycle(6) # tested worked.
+    
+
+time.sleep(5)
+    #p.ChangeDutyCycle(0)                
+    #time.sleep(0.2)
+     
+p.stop()
+GPIO.cleanup()
